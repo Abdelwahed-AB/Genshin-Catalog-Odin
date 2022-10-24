af69076993c269e7251985d7b10cb6b242a72603
Abdelwahed AB <ababdelwahed112@gmail.com>
2022-10-24 20:18:37 +0100

    Add create weapon

--- controllers/weaponController.js
+++ controllers/weaponController.js
@@ -1,6 +1,9 @@
 const Weapon = require("../models/weapon");
 const WeaponType = require("../models/weaponType");
 const async = require("async");
+const {body, validationResult} = require("express-validator");
+const he = require("he");
+const weapon = require("../models/weapon");
 
 
 exports.weapon_list = (req, res, next)=>{
@@ -44,12 +47,61 @@ exports.weapon_detail = (req, res, next)=>{
     });
 };
 
-exports.weapon_create_get = ()=>{
-    return "Not yet implemented.";
+exports.weapon_create_get = (req, res, next)=>{
+    WeaponType.find().exec((err, results)=>{
+        if(err) return next(err);
+
+        res.render("weapon_form", {
+            weapon_types: results, 
+            weapon: {},
+        })
+    });
 };
-exports.weapon_create_post = ()=>{
-    return "Not yet implemented.";
-};
+exports.weapon_create_post = [
+    body("name").trim().isLength({min: 1}).escape().withMessage("Name must be specified."),
+    body("rarity").isInt().withMessage("You must pick a rarity."),
+    body("base_attack").isInt().withMessage("Base attack must be specified."),
+    body("weapon_type").isLength({min: 1}).withMessage("You must pick a weapon type."),
+    body("passive").trim().escape().isLength({min: 1}).withMessage("Weapon passive must be specified."),
+    body("second_stat").trim().escape().isLength({min: 1}).withMessage("Secondary stat must be specified."),
+    body("img").trim().isLength({min: 1}).withMessage("Thumbnail image must be specified."),
+
+    (req, res, next)=>{
+        let errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            WeaponType.find().exec((err, results)=>{
+                if(err) return next(err);
+                
+                let weapon = req.body;
+                weapon.name = he.decode(weapon.name);
+                weapon.ima = he.decode(weapon.img);
+                weapon.passive = he.decode(weapon.passive);
+                res.render("weapon_form", {
+                    weapon_types: results, 
+                    weapon: weapon,
+                    errors: errors.array(),
+                })
+            });
+
+            return;
+        }
+
+        let weapon = req.body;
+        weapon.name = he.decode(weapon.name);
+        weapon.ima = he.decode(weapon.img);
+        weapon.passive = he.decode(weapon.passive);
+        weapon.second_stat = he.decode(weapon.second_stat);
+
+        weapon = new Weapon(weapon);
+
+        weapon.save((err, newWeap)=>{
+            if(err) return next(err);
+
+            res.redirect("/weapon/"+newWeap._id);
+        });
+    },
+];
 
 exports.weapon_delete_get = ()=>{
     return "Not yet implemented.";
--- models/weapon.js
+++ models/weapon.js
@@ -6,7 +6,6 @@ const WeaponSchema = new Schema({
     base_attack: {type: Number, required: true},
     second_stat: {type: String, required: true, maxLength: 100},
     passive: {type: String, required: true, maxLength: 500},
-    description: {type: String, required: true},
     rarity: {type: Number, min: 1, max: 5},
     weapon_type: {type: mongoose.Types.ObjectId},
     img: {type: String, required: true},
